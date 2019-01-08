178b4a93fcfb2e94cd3c9dfa7ae84629e6017996
uda1099 <dudizimber@hotmail.com>
2019-01-08 16:45:54 +0200

    Update euclideanDistance.ts

--- src/euclideanDistance.ts
+++ src/euclideanDistance.ts
@@ -11,3 +11,15 @@ export function euclideanDistance(arr1: number[] | Float32Array, arr2: number[] 
       .reduce((res, diff) => res + Math.pow(diff, 2), 0)
   )
 }
+
+export function euclideanDistanceSquared(arr1: number[] | Float32Array, arr2: number[] | Float32Array) {
+  if (arr1.length !== arr2.length)
+    throw new Error('euclideanDistance: arr1.length !== arr2.length')
+
+  const desc1 = Array.from(arr1)
+  const desc2 = Array.from(arr2)
+
+  return desc1
+          .map((val, i) => val - desc2[i])
+          .reduce((res, diff) => res + Math.pow(diff, 2), 0)
+}
